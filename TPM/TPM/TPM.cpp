1862b434ef566ea49c4fe25c38f56b8c91ac1785
Vladislav Chibisov <lapwing61@gmail.com>
2016-09-11 20:59:04 +0800

    frirst

---
+++ TPM/TPM/TPM.cpp
@@ -0,0 +1,24 @@
+// TPM.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+
+	char x;
+
+	boost::property_tree::ptree pt;
+	boost::property_tree::ini_parser::read_ini("d:\\tmp\\config.ini", pt);
+
+	cout << pt.get<string>("Provider.id") << endl;
+	cout << pt.get<string>("Provider.key") << endl;
+	cout << pt.get<string>("Output.type") << endl;
+	cout << pt.get<string>("Output.dir") << endl;
+
+	cin >> x;
+
+	return 0;
+}
+
---
+++ TPM/TPM/stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
